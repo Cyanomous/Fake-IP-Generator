6ce243ae0de4f2e3eefc4369185239eae8540e24
Cyan <69328940+Cyanomous@users.noreply.github.com>
2021-04-02 11:57:00 -0400

    Added more stuff

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Fake IP Generator
 
 Just makes realistic looking fake ip addresses
+
+You will need:
+- Python installed
+- These python packages:
+    - json
+    - random
+
+Also, you can view the IP address in: "json/ip_address.json"
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import random
+import json
 
 fake_ip_address = f"{random.randrange(1, 99)}.{random.randrange(1, 999)}.{random.randrange(1, 999)}.{random.randrange(1, 999)}"
 
-print(fake_ip_address)
+with open('json/ip_address.json', 'r') as f:
+    ip = json.load(f)
+ip["ip_address"] = fake_ip_address
+with open('json/ip_address.json', 'w') as f:
+    json.dump(ip, f, indent=4)
+    print(fake_ip_address)
